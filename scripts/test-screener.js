e4d1834e32f3071bcf733f34c4f8e01dc189f000
James Leigh <james@3roundstones.com>
2014-05-02 20:44:48 -0400

    Adjust filing delay to one month after year-end

--- scripts/test-screener.js
+++ scripts/test-screener.js
@@ -288,9 +288,9 @@ describe("Screener", function(){
                 3, 'm12', new Date(2014, 3, 1),
                 [
                     // annual financials are expected at the end of the following two months
-                    [new Date(2012, 2, 1),631,71.15,138,21.92,98,1.92,0.15,7.8,51,17.08,165,-23,142,2.78,341,100,28.85,71.15,34.02,8.42,6.8,21.92,0.27,22.19,31.16,15.58,0.56,8.71,1.37,12.03,12.03,undefined,13.69,7.91,14.91,21.34,14.34,-0.17,12.29,undefined,13.88,2.06,13.7,undefined,12.94,0.7,11.58,undefined,33.67,30.38,3.69,22.43,1.44,40.12,10.3,undefined,1.8,52.22,5.82,39.1,2.86,100,undefined,undefined,undefined,6.24,16.85,23.09,undefined,3.93,27.02,72.98,100,2.26,2.18,1.37,undefined,64.8,undefined,undefined,undefined,5.63,undefined,9.69,0.56],
-                    [new Date(2013, 2, 1),658,70.32,151,22.89,108,2.2,0.53,25,49,15.59,146,-30,116,2.36,217,100,29.68,70.32,33.08,7.81,6.55,22.89,0.45,23.34,34.42,16.42,0.59,9.76,1.44,13.67,13.67,undefined,4.26,11.18,8.63,19.63,8.85,6.33,5.14,undefined,9.88,9.44,7.89,73.16,14.58,9.84,7.53,undefined,-11.5,-18.14,4.56,17.61,1.07,30.85,12.34,undefined,2.36,45.55,8.06,42,4.39,100,undefined,undefined,undefined,6.46,18.24,24.7,undefined,5.67,30.37,69.63,100,1.84,1.75,1.44,undefined,63.12,undefined,undefined,undefined,5.78,undefined,8.65,0.59],
-                    [new Date(2014, 2, 1),698,61.13,171,24.44,124,2.66,0.38,14.1,46,15.35,187,-34,153,3.29,167,100,38.87,61.13,30.14,undefined,6.54,24.44,1.05,25.49,31.48,17.69,0.67,11.92,1.49,17.45,17.45,undefined,6.07,7.93,6.8,17.48,13.26,12.13,4.17,undefined,14.3,12.67,5.95,undefined,undefined,undefined,undefined,undefined,27.85,32.01,4.81,21.92,1.24,28.97,11.46,undefined,2.94,43.36,10.19,41.76,4.7,100,undefined,undefined,undefined,6.93,20.27,27.2,undefined,5.83,33.03,66.97,100,1.59,1.49,1.49,undefined,59.72,undefined,undefined,undefined,6.11,undefined,7.39,0.67]
+                    [new Date(2012, 1, 1),631,71.15,138,21.92,98,1.92,0.15,7.8,51,17.08,165,-23,142,2.78,341,100,28.85,71.15,34.02,8.42,6.8,21.92,0.27,22.19,31.16,15.58,0.56,8.71,1.37,12.03,12.03,undefined,13.69,7.91,14.91,21.34,14.34,-0.17,12.29,undefined,13.88,2.06,13.7,undefined,12.94,0.7,11.58,undefined,33.67,30.38,3.69,22.43,1.44,40.12,10.3,undefined,1.8,52.22,5.82,39.1,2.86,100,undefined,undefined,undefined,6.24,16.85,23.09,undefined,3.93,27.02,72.98,100,2.26,2.18,1.37,undefined,64.8,undefined,undefined,undefined,5.63,undefined,9.69,0.56],
+                    [new Date(2013, 1, 1),658,70.32,151,22.89,108,2.2,0.53,25,49,15.59,146,-30,116,2.36,217,100,29.68,70.32,33.08,7.81,6.55,22.89,0.45,23.34,34.42,16.42,0.59,9.76,1.44,13.67,13.67,undefined,4.26,11.18,8.63,19.63,8.85,6.33,5.14,undefined,9.88,9.44,7.89,73.16,14.58,9.84,7.53,undefined,-11.5,-18.14,4.56,17.61,1.07,30.85,12.34,undefined,2.36,45.55,8.06,42,4.39,100,undefined,undefined,undefined,6.46,18.24,24.7,undefined,5.67,30.37,69.63,100,1.84,1.75,1.44,undefined,63.12,undefined,undefined,undefined,5.78,undefined,8.65,0.59],
+                    [new Date(2014, 1, 1),698,61.13,171,24.44,124,2.66,0.38,14.1,46,15.35,187,-34,153,3.29,167,100,38.87,61.13,30.14,undefined,6.54,24.44,1.05,25.49,31.48,17.69,0.67,11.92,1.49,17.45,17.45,undefined,6.07,7.93,6.8,17.48,13.26,12.13,4.17,undefined,14.3,12.67,5.95,undefined,undefined,undefined,undefined,undefined,27.85,32.01,4.81,21.92,1.24,28.97,11.46,undefined,2.94,43.36,10.19,41.76,4.7,100,undefined,undefined,undefined,6.93,20.27,27.2,undefined,5.83,33.03,66.97,100,1.59,1.49,1.49,undefined,59.72,undefined,undefined,undefined,6.11,undefined,7.39,0.67]
                 ]
             ]
         ], loadQuotes);
@@ -330,7 +330,7 @@ describe("Screener", function(){
             ['XNYS', 'C^K', ['date(asof)', 'revenue_mil'],
                 1, 'm12', new Date(2014, 3, 1),
                 [
-                    [new Date(2014, 2, 1), 76366]
+                    [new Date(2014, 1, 1), 76366]
                 ]
             ]
         ], loadQuotes);
@@ -338,15 +338,15 @@ describe("Screener", function(){
             ['XASE', 'ADK^A', ['date(asof)', 'revenue_mil'],
                 1, 'm12', new Date(2014, 3, 1),
                 [
-                    [new Date(2014, 2, 1), 223]
+                    [new Date(2014, 1, 1), 223]
                 ]
             ]
         ], loadQuotes);
         these("should find AA^ morningstar symbol", [
             ['XASE', 'AA^', ['date(asof)', 'revenue_mil'],
-                1, 'm12', new Date(2014, 3, 1),
+                1, 'm12', new Date(2014, 2, 1),
                 [
-                    [new Date(2014, 2, 1), 23032]
+                    [new Date(2014, 1, 1), 23032]
                 ]
             ]
         ], loadQuotes);
