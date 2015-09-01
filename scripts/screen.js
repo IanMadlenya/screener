64272788b9594ee8b002d0405c96a13cf86a5600
James Leigh <james@leighnet.ca>
2015-09-01 15:47:36 -0400

    Don't show large exposed growth values in screen

--- scripts/screen.js
+++ scripts/screen.js
@@ -489,7 +489,7 @@ jQuery(function($){
         $('#performance_factor').text(loosers.length ? (sum(winners) / -sum(loosers)).toFixed(1) : '');
         $('#performance').text(performance.toFixed(2) + '%');
         $('#annual_growth').text((growth * 100).toFixed(2) + '%');
-        $('#exposed_growth').text((exposed_growth * 100).toFixed(2) + '%');
+        $('#exposed_growth').text(Math.abs(exposed_growth) < 1000 ? (exposed_growth * 100).toFixed(2) + '%' : '');
         return list;
     }
 
